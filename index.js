7e02b4384f45375b08b90d902a9810afab57baf9
Paul Obunga <paulobunga.one@gmail.com>
2020-05-01 13:46:00 +0300

    Updated Code

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-import { NativeModules } from 'react-native';
+import { DeviceEventEmitter, NativeModules } from 'react-native';
 
 const { C72RfidScanner } = NativeModules;
+
+powerStatusListener = null;
+tagDataListener = null;
+
+export const startListeningForPowerStatus = (powerStatusChangeCallback) => {
+    powerStatusListener = DeviceEventEmitter.addListener('UHF_POWER', powerStatusChangeCallback);
+}
+
+export const stopListeningForPowerStatus = () => {
+    powerStatusListener.remove();
+}
+
+export const changeListenerCallback = (processTagCallback) => {
+    if(tagDataListener)
+        tagDataListener.remove()
+    tagDataListener = DeviceEventEmitter.addListener('UHF_TAG', processTagCallback);
+}
+
+export const startListeningForTags = (processTagCallback) => {
+    tagDataListener = DeviceEventEmitter.addListener('UHF_TAG', processTagCallback);
+}
+export const stopListeningForTags = () => {
+    tagDataListener.remove();
+}
 
 export default C72RfidScanner;
