3a0da531a1650d85adb0f527d3e055da4232381b
Paul Obunga <paulobunga.one@gmail.com>
2020-06-15 18:41:48 +0300

    Fixed bug: Can't Rescan after tags are cleared

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,59 @@
-import { NativeEventEmitter, NativeModules } from 'react-native';
+import { NativeEventEmitter, NativeModules } from "react-native";
 
 const { C72RfidScanner } = NativeModules;
 
 const eventEmitter = new NativeEventEmitter(C72RfidScanner);
 
-type initializeReader = () => void
+type initializeReader = () => void;
 
-type deInitializeReader = () => void
+type deInitializeReader = () => void;
 
-type AddListener = (cb: (args: any[]) => void) => void
+type readSingleTag = () => Promise<any>;
 
-export const initializeReader: initializeReader = () => C72RfidScanner.initializeReader();
+type readPower = () => Promise<any>;
 
-export const deInitializeReader: deInitializeReader  = () => C72RfidScanner.deInitializeReader();
+type changePower = (powerValue: any) => Promise<any>;
+
+type AddListener = (cb: (args: any[]) => void) => void;
+
+type clearTags = () => void;
+
+export const initializeReader: initializeReader = () =>
+  C72RfidScanner.initializeReader();
+
+export const deInitializeReader: deInitializeReader = () =>
+  C72RfidScanner.deInitializeReader();
+
+export const readSingleTag: readSingleTag = () =>
+  C72RfidScanner.readSingleTag();
+
+export const startReadingTags = (callback: (args: any[]) => any) =>
+  C72RfidScanner.startReadingTags(callback);
+
+export const stopReadingTags = (callback: (args: any[]) => any) =>
+  C72RfidScanner.stopReadingTags(callback);
+
+export const readPower = () => C72RfidScanner.readPower();
+
+export const changePower: changePower = (powerValue: any) =>
+  C72RfidScanner.changePower(powerValue);
 
 export const powerListener: AddListener = (listener) =>
-    eventEmitter.addListener("UHF_POWER", listener);
+  eventEmitter.addListener("UHF_POWER", listener);
 
 export const tagListener: AddListener = (listener) =>
-    eventEmitter.addListener("UHF_TAG", listener);
+  eventEmitter.addListener("UHF_TAG", listener);
+
+export const clearTags: clearTags = () => C72RfidScanner.clearAllTags();
 
 export default {
-    initializeReader,
-    deInitializeReader
-}
+  powerListener,
+  tagListener,
+  initializeReader,
+  readSingleTag,
+  startReadingTags,
+  stopReadingTags,
+  readPower,
+  changePower,
+  deInitializeReader,
+};
